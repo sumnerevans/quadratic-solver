73072f8c926a944ff9174a1a59678c8fe6e530bc
Sumner Evans <sumner.evans98@gmail.com>
2016-11-28 10:12:56 -0700

    Removed inside.mines.edu deploy

--- deploy.sh
+++ deploy.sh
@@ -11,10 +11,3 @@ scp -r builds/* tef:/home/evansfamilywebsite/the-evans.family/sumner/quadratic-s
 echo ""
 echo "Deploy Successful!"
 
-echo ""
-echo "========== Deploying to inside.mines.edu/~jonathanevans =========="
-ssh illuminate "mkdir -p /u/eu/an/jonathanevans/public_html/quadratic-solver"
-scp -r builds/* illuminate:/u/eu/an/jonathanevans/public_html/quadratic-solver
-echo ""
-echo "Deploy Successful!"
-
